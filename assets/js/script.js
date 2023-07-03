cbb7ef04dae6900ce6a2089e5f032b5c25e2fa1a
Jack <131559096+jackjg123@users.noreply.github.com>
2023-07-03 19:29:02 -0400

    added enter key functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,7 @@ function getCity() {
           console.log(data);
           console.log(data.dt);
           updateWeatherData(data);
+          showWeather();
         });
     });
 }
@@ -46,25 +47,44 @@ function getCity() {
 function updateWeatherData(data) {
   var cityName = data.name;
 
-  //need to convert dt from seconds to milliseconds for the toLocalDateString
+  //convert milliseconds to todays date
   var currentDate = new Date(data.dt * 1000);
   var kelvinTemperature = data.main.temp;
+
+  //convert kelvin temp to farenheit and round to nearest tenth
   var temperature = Math.floor(kelvinTemperature - 273) * (9 / 5) + 32;
+  temperature = temperature.toFixed(1);
+
   var windSpeed = data.wind.speed;
   var humidity = data.main.humidity;
   currentDate = currentDate.toLocaleString();
 
-  console.log('City: ' + cityName);
-  console.log('date in milliseconds: ' + currentDate);
-  console.log('temperature: ' + temperature);
-  console.log('wind speed: ' + windSpeed);
-  console.log('humidity: ' + humidity);
+  console.log('Here are your results:');
+  console.log('   City: ' + cityName);
+  console.log('   Date: ' + currentDate);
+  console.log('   Temperature: ' + temperature + ' F');
+  console.log('   Wind Speed: ' + windSpeed + ' mph');
+  console.log('   Humidity: ' + humidity + '%');
+
+  // Display the values of the variables to the screen.
   $('#current-city h3').text(cityName + '-' + currentDate);
   $('#current-city p:nth-of-type(1)').text('Temp: ' + temperature + ' F');
-  $('#current-city p:nth-of-type(2)').text('Wind speed: ' + windSpeed + ' mph');
+  $('#current-city p:nth-of-type(2)').text('Wind speed: ' + windSpeed + ' Mph');
   $('#current-city p:nth-of-type(3)').text('Humidity: ' + humidity + '%');
+}
+
+//Unhide the divs displaying the weather.
+function showWeather() {
+  $('#current-city').css('display', 'block');
+  $('#five-day-wind').css('display', 'block');
 }
 
 $('#search-btn').click(function () {
   getCity();
+});
+$('#search').keydown(function (event) {
+  if (event.keyCode === 13) {
+    getCity();
+    event.preventDefault();
+  }
 });
